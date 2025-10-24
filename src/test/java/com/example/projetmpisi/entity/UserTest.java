56b6e4e7b42314e00d65ac199441447df07fce17
Ghofrane <ghofranedhouibi2002@gmail.com>
2025-10-24 12:01:18 +0100

    first commit

---
+++ src/test/java/com/example/projetmpisi/entity/UserTest.java
@@ -0,0 +1,32 @@
+package com.example.projetmpisi.entity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getUsername() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void setUsername() {
+    }
+
+    @Test
+    void setEmail() {
+    }
+}
