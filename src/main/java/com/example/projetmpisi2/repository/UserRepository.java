296f1e37f5b392ae49d7add66cd7af79f065827d
Amalab1 <amalabdaoui20@gmail.com>
2026-01-17 19:43:42 +0100

    Ajout du pipeline CI/CD avec SonarCloud et Docker

--- src/main/java/com/example/projetmpisi2/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.projetmpisi2.repository;
-
-import com.example.projetmpisi2.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
